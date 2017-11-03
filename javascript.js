6331aaa6916f2030f138f5773d8633279068d65d
jddg5wa <jddg5wa@gmail.com>
2017-11-03 04:56:56 -0700

    Added comments

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 var locations = ["Anarchy Acres", "Fatal Fields","Flush Factory","Greasy Grove","Lonely Lodge","Loot Lake","Moisty Mire","Pleasant Park","Retail Row","Wailing Woods"];
 
 function getRandomLocation(){
-    if (Math.random() < .5){
+    if (Math.random() < .5) // half the time give a location between two map locations
+    {
         var randLoc1 = locations[Math.floor(Math.random()* locations.length)];
         var randLoc2 = locations[Math.floor(Math.random()* locations.length)];
         while (randLoc1 == randLoc2){
@@ -9,7 +10,8 @@ function getRandomLocation(){
         }
         document.getElementById('outputText').innerHTML = "inbetween <u>" + randLoc1 + "</u> and <u>" + randLoc2 + "</u>";
     }
-    else {
+    else // otherwise give map location
+    {
         var randLoc = locations[Math.floor(Math.random()* locations.length)];
         document.getElementById('outputText').innerHTML = "<u>" + randLoc + "</u>";
     }
